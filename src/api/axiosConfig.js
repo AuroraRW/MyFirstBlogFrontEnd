dc230261e4505b34c817d23d605862e5a1fd6d75
gpoquendo <againstthaflow13@protonmail.com>
2024-07-15 21:06:50 -0600

    update axiosConfig.js to use the correct environment variable for the server URL

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,11 +1,14 @@
-import axios from "axios"
+import axios from 'axios'
 import { isHyperlink } from '@/lib/isHyperlink'
 
-const BASE_URL = process.env.DOTNET_SERVER_URL
+const BASE_URL = process.env.NEXT_PUBLIC_DOTNET_SERVER_URL
 
 const AXIOS_BASE = axios.create({
-    baseURL: BASE_URL,
-  })
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
 
 const JSON_CLIENT = isHyperlink(BASE_URL) ? AXIOS_BASE : false
 
