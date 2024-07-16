b3c3915e5bf51f4a8efa2378a4c3b879f154b02d
gpoquendo <againstthaflow13@protonmail.com>
2024-07-15 21:07:17 -0600

    Refactor postsApi.js for improved readability and error handling

--- src/api/postsApi.js
+++ src/api/postsApi.js
@@ -2,10 +2,8 @@ import API from './axiosConfig'
 
 export const getPosts = () => {
   try {
-    return API.get('/posts/')
-      .then((res) => res.data)
-  }
-  catch (e) {
+    return API.get('/posts/').then((res) => res.data)
+  } catch (e) {
     console.error(e)
     return []
   }
@@ -13,11 +11,22 @@ export const getPosts = () => {
 
 export const getPost = (postSlug) => {
   try {
-    return API.get(`/posts/${postSlug}`)
-      .then((res) => res.data)
-  }
-  catch (e) {
+    return API.get(`/posts/${postSlug}`).then((res) => res.data)
+  } catch (e) {
     console.error(e)
     return {}
+  }
+}
+
+export const createPost = async (post) => {
+  try {
+    console.log('Creating post:', post)
+    const response = await API.post('/posts', post)
+    console.log('Post created:', response.data)
+    //await new Promise((resolve) => setTimeout(resolve, 1000))
+    return response.data.post
+  } catch (error) {
+    console.error('Error creating post:', error.response?.data || error.message)
+    throw error
   }
 }
